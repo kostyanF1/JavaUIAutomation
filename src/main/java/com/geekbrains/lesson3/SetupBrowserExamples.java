79a6813544af818d27c00c9565d96c60fe2e73c1
kostyanF1 <konstantindragomirov85@gmail.com>
2021-11-17 19:46:08 +0500

    Initial commit

---
+++ src/main/java/com/geekbrains/lesson3/SetupBrowserExamples.java
@@ -0,0 +1,32 @@
+package com.geekbrains.leson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetupBrowserExamples {
+    public static void main (String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox")
+                .addArguments("--disable-notification")
+                .addArguments("user-agent=Googlebot/2.1 (+http://www.google.com/bot.html)");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        Thread.sleep(5000);
+
+
+        ((JavascriptExecutor) driver).executeScript("window.open()");
+        Thread.sleep(5000);
+        List<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(0));
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
+}
